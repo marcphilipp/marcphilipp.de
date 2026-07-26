21a8f710f7e482aff053c828472a70fb1f114723
Marc Philipp <mail@marcphilipp.de>
2026-07-26 17:52:19 +0200

    Use HTTP redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://marcphilipp.de',
   trailingSlash: 'always',
-  redirects: {
-    '/en/about-me/': '/',
-    '/de/ueber-mich/': '/de/startseite/',
-  },
   markdown: {
     shikiConfig: {
       theme: 'nord',
