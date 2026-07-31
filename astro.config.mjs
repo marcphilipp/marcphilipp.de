13ef23923441ac3fa18b8b1311a93d156efb4487
Marc Philipp <mail@marcphilipp.de>
2026-07-31 20:33:13 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://marcphilipp.de',
   trailingSlash: 'always',
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    sitemap({ filter: (page) => !page.includes('/impressum/') }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'nord',
